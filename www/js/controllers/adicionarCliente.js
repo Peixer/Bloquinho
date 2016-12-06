d3296492be025207fc2ef7450bd9aa0d8738c583
Glaicon <gjpeixer@hotmail.com>
2016-12-05 23:34:48 -0200

    Zerar cliente após gravar cliente, na tela de adicionarCliente.

--- www/js/controllers/adicionarCliente.js
+++ www/js/controllers/adicionarCliente.js
@@ -1,64 +1,69 @@
 angular.module('starter.controllers')
-    .controller('AdicionarClienteCtrl', [
-        '$scope', '$state', '$repositorio', '$ionicPopup', '$stateParams',
-        function ($scope, $state, $repositorio, $ionicPopup, $stateParams) {
+  .controller('AdicionarClienteCtrl', [
+    '$scope', '$state', '$repositorio', '$ionicPopup', '$stateParams',
+    function ($scope, $state, $repositorio, $ionicPopup, $stateParams) {
 
-            $scope.cliente = {}
+      $scope.cliente = {}
 
-            inicializarCliente();
+      inicializarCliente();
 
-            $scope.adicionarCliente = function () {
+      $scope.adicionarCliente = function () {
 
-                if (validarCampos()) {
-                    $repositorio.gravarCliente($scope.cliente);
+        if (validarCampos()) {
+          $repositorio.gravarCliente($scope.cliente);
 
-                    $state.go('tab.clientes');
-                }
-            };
-
-            $scope.limpar = function () {
-                inicializarCliente();
-            };
-
-            $scope.voltar = function () {
-                $state.go('tab.clientes');
-            };
-
-            function validarCampos() {
-
-                if ($scope.cliente.nome == '') {
-                    $ionicPopup.alert({
-                        title: 'Atenção',
-                        template: 'Nome está inválido'
-                    });
-
-                    return false;
-                } else if ($scope.cliente.email == '') {
-                    $ionicPopup.alert({
-                        title: 'Atenção',
-                        template: 'Email está inválido'
-                    });
-
-                    return false;
-                }
-
-                return true;
-            };
-
-            function inicializarCliente() {
-                if ($stateParams.id != '') {
-                    $repositorio.obterClienteComId($stateParams.id).then(function (doc) {
-                        $scope.cliente = doc;
-                    });
-                } else {
-                    $scope.cliente = {
-                        total: 0,
-                        telefone: '',
-                        email: '',
-                        nome: '',
-                        endereco: ''
-                    };
-                }
-            };
+          zerarCliente();
+          $state.go('tab.clientes');
         }
-    ]);
+      };
+
+      $scope.limpar = function () {
+        inicializarCliente();
+      };
+
+      $scope.voltar = function () {
+        $state.go('tab.clientes');
+      };
+
+      function validarCampos() {
+
+        if ($scope.cliente.nome == '') {
+          $ionicPopup.alert({
+            title: 'Atenção',
+            template: 'Nome está inválido'
+          });
+
+          return false;
+        } else if ($scope.cliente.email == '') {
+          $ionicPopup.alert({
+            title: 'Atenção',
+            template: 'Email está inválido'
+          });
+
+          return false;
+        }
+
+        return true;
+      };
+
+      function inicializarCliente() {
+        if ($stateParams.id != '') {
+          $repositorio.obterClienteComId($stateParams.id).then(function (doc) {
+            $scope.cliente = doc;
+          });
+        } else {
+          zerarCliente();
+        }
+      };
+
+      function zerarCliente() {
+        $scope.cliente = {
+          total: 0,
+          telefone: '',
+          email: '',
+          nome: '',
+          endereco: ''
+        };
+      };
+    }
+  ]);
