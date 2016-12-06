d69d0b432af80a5a109c9d1f7e44ed20178a277f
Glaicon <gjpeixer@hotmail.com>
2016-12-05 23:34:19 -0200

    Removendo método desnecessário no repositorio.js

--- www/js/servico/repositorio.js
+++ www/js/servico/repositorio.js
@@ -1,75 +1,60 @@
 angular.module('starter.services')
-    .service('$repositorio', [function () {
-        var db;
+  .service('$repositorio', [function () {
+    var db;
 
-        function inicializarDB() {
-            db = new PouchDB('bloquinho');
-        };
+    function inicializarDB() {
+      db = new PouchDB('bloquinho');
+    };
 
-        if (!db)
-            inicializarDB();
+    if (!db)
+      inicializarDB();
 
-        this.listarClientes = function () {
-            var items = [];
+    this.listar = function () {
+      return db.allDocs({
+        include_docs: true
+      });
+    };
 
-            db.allDocs({
-                include_docs: true
-            }).then(function (result) {
-                angular.forEach(result.rows, function (value, key) {
-                    items.push(value.doc);
-                });
-            }).catch(function (error) {
-                console.log(error);
-            });
+    this.gravarCliente = function (cliente) {
+      if (cliente._id != null) {
+        return atualizarCliente(cliente);
+      } else {
+        return adicionarCliente(cliente);
+      }
+    };
 
-            return items;
-        };
+    function adicionarCliente(cliente) {
+      return db.post({
+        nome: cliente.nome,
+        total: cliente.total,
+        endereco: cliente.endereco,
+        email: cliente.email,
+        telefone: cliente.telefone
+      });
+    };
 
-        this.listar = function () {
-            return db.allDocs({
-                include_docs: true
-            });
-        };
+    function atualizarCliente(cliente) {
+      return db.put({
+        _id: cliente._id,
+        _rev: cliente._rev,
+        nome: cliente.nome,
+        total: cliente.total,
+        endereco: cliente.endereco,
+        email: cliente.email,
+        telefone: cliente.telefone,
+        movimentacoes: cliente.movimentacoes
+      });
+    };
 
-        this.gravarCliente = function (cliente) {
-            if (cliente._id != null) {
-                return atualizarCliente(cliente);
-            } else {
-                return adicionarCliente(cliente);
-            }
-        };
+    this.deletarCliente = function (item) {
+      db.remove(item._id, item._rev);
+    };
 
-        function adicionarCliente(cliente) {
-            return db.post({
-                nome: cliente.nome,
-                total: cliente.total,                
-                endereco: cliente.endereco,
-                email: cliente.email,
-                telefone: cliente.telefone            });
-        };
+    this.obterClienteComId = function (id) {
+      return db.get(id);
+    };
 
-        function atualizarCliente(cliente) {
-            return db.put({
-                _id: cliente._id,
-                _rev: cliente._rev,
-                nome: cliente.nome,
-                total: cliente.total,
-                endereco: cliente.endereco,
-                email: cliente.email,
-                telefone: cliente.telefone,
-                movimentacoes: cliente.movimentacoes
-            });
-        };
-
-        this.deletarCliente = function (item) {
-            db.remove(item._id, item._rev);
-        };
-
-        this.obterClienteComId = function (id) {
-            return db.get(id);
-        };
-
-    }]);
+  }]);
 
 //todo como deveria ficar o registro de cliente
 // {
