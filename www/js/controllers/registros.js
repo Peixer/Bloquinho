99ed61013c96a9daa8a24097113e60975d72ed03
Glaicon <gjpeixer@hotmail.com>
2016-12-06 19:57:24 -0200

    Renomeado repositorio para repositorioDAO.

--- www/js/controllers/registros.js
+++ www/js/controllers/registros.js
@@ -1,36 +1,41 @@
 angular.module('starter.controllers')
-    .controller('RegistrosCtrl', [
-        '$scope', '$repositorio', '$ionicLoading', function ($scope, $repositorio, $ionicLoading) {
+  .controller('RegistrosCtrl', [
+    '$scope', '$repositorio',
+    function ($scope, $repositorio) {
 
-            $scope.registros = [];
-            inicializar();
+      $scope.registros = [];
+      inicializar();
 
-            function inicializar() {
-                $ionicLoading.show({
-                    template: '<ion-spinner icon="android"></ion-spinner>',
-                    hideOnStageChange: true
-                });
+      function inicializar() {
 
-                var clientes = $repositorio.listar().then(function (clientes) {
+        var clientes = $repositorio.listar().then(function (clientes) {
 
-                    angular.forEach(clientes.rows, function (cliente, index) {
-                        angular.forEach(cliente.doc.movimentacoes, function (registro, indexRegistro) {
+          angular.forEach(clientes.rows, function (cliente, index) {
+            angular.forEach(cliente.doc.movimentacoes, function (registro, indexRegistro) {
 
-                            var registroDoCliente = {
-                                nome: cliente.doc.nome,
-                                valor: registro.valor,
-                                descricao: registro.descricao,
-                                ehEntrada: registro.ehEntrada,
-                                data: registro.data
-                            };
+              var registroDoCliente = {
+                nome: cliente.doc.nome,
+                valor: registro.valor,
+                descricao: registro.descricao,
+                ehEntrada: registro.ehEntrada,
+                data: registro.data
+              };
 
-                            $scope.registros.push(registroDoCliente);
-                        });
-                    });
-                    
-                    console.log( $scope.registros);
-                    $ionicLoading.hide();
-                });
-            };
+              $scope.registros.push(registroDoCliente);
+            });
+          });
 
-        }]);
+          console.log($scope.registros);
+          $scope.$broadcast('scroll.refreshComplete');
+        }, function (data) {
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+      };
+
+      $scope.refresh = function () {
+         $scope.registros = [];
+        inicializar();
+      };
+
+    }
+  ]);
